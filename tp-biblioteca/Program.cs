103f408536712bd201d41c34542e9df62a409e5e
Matias Perez <matiperez881@gmail.com>
2025-04-21 19:15:22 -0300

    Se agregaron las clases Biblioteca y Libro, junto con sus atributos y la clase principal tal como estaban en la teoria de la semana 5.

--- tp-biblioteca/Program.cs
+++ tp-biblioteca/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using tp_biblioteca.entidades;
+
+namespace Colecciones
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            Biblioteca biblioteca = new Biblioteca();
+            cargarLibros(10);
+            cargarLibros(2);
+            biblioteca.listarLibros();
+            biblioteca.eliminarLibro("Libro5");
+            biblioteca.listarLibros();
+
+            void cargarLibros(int cantidad)
+            {
+                bool pude;
+                for (int i = 1; i <= cantidad; i++)
+                {
+                    pude = biblioteca.agregarLibro("Libro" + i, "Autor" + i, "Editorial" + i);
+                    if (pude)
+                    {
+                        Console.WriteLine("Libro" + i + "agregado correctamente");
+                    }
+                    else
+                    {
+                        Console.WriteLine("libro" + i + " Ya existe en la biblioteca");
+                    }
+                }
+            }
+        }
+    }
+}
