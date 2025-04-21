1f166414873b16f84dca5578b1db5d45d46885c9
Matias Perez <matiperez881@gmail.com>
2025-04-21 19:26:36 -0300

    Incorporacion de entidad lector con sus atributos y constructor

---
+++ tp-biblioteca/entidades/Lector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp_biblioteca.entidades
+{
+    internal class Lector
+    {
+        private string nombre;
+        private string dni;
+        private List<Libro> librosPrestados;
+
+        public Lector(string nombre, string dni)
+        {
+            this.nombre = nombre;
+            this.dni = dni;
+            this.librosPrestados = [];
+        }
+    }
+}
