647e161dad0e9e34addda862462b71a5d396549d
Matias Perez <matiperez881@gmail.com>
2025-04-21 19:15:22 -0300

    Se agregaron las clases Biblioteca y Libro, junto con sus atributos y la clase principal tal como estaban en la teoria de la semana 5.

---
+++ tp-biblioteca/entidades/Biblioteca.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp_biblioteca.entidades
+{
+    internal class Biblioteca
+    {
+        private List<Libro> libros;
+
+        public Biblioteca()
+        {
+            this.libros = new List<Libro>();
+        }
+
+        private Libro buscarLibro(string titulo)
+        {
+            Libro libroBuscado = null;
+            int i = 0;
+            while (i < libros.Count && !libros[i].getTitulo().Equals(titulo))
+            {
+                i++;
+            }
+            if (i != libros.Count)
+            {
+                libroBuscado = libros[i];
+            }
+            return libroBuscado;
+        }
+
+        public bool agregarLibro(string titulo, string autor, string editorial)
+        {
+            bool resultado = false;
+            Libro libro;
+            libro = buscarLibro(titulo);
+            if (libro == null)
+            {
+                libro = new Libro(titulo, autor, editorial);
+                libros.Add(libro);
+                resultado = true;
+            }
+            return resultado;
+        }
+
+        public void listarLibros()
+        {
+            foreach (var libro in libros)
+            {
+                Console.WriteLine(libro);
+            }
+        }
+
+        public bool eliminarLibro(string titulo)
+        {
+            bool resultado = false;
+            Libro libro;
+            libro = buscarLibro(titulo);
+            if (libro != null)
+            {
+                libros.Remove(libro);
+                resultado = true;
+            }
+            return resultado;
+        }
+    }
+}
